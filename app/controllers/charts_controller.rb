99f9008ee8459e62f6ae6067ff72ff89d12f3b1b
pp <zjinys@gmail.com>
2010-07-13 12:59:24 +0800

    fix the issue_day_create_status chart

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -17,7 +17,7 @@ class ChartsController < ApplicationController
                     :data => issueinfo[0],
                     :legend => issueinfo[1],
                     :axis_labels => issueinfo[2],
-                    :axis_with_labels => 'x,y'
+                    :axis_with_labels => 'x'
             )
     
 
@@ -75,7 +75,6 @@ class ChartsController < ApplicationController
     end
 
 
-
     axis_labels = axis_labels.reverse
     
     authors = Issue.find_by_sql ['select distinct i.author_id author_id,CONCAT(u.firstname,u.lastname) name from issues i,users u where i.author_id = u.id and to_days(now())-to_days(i.created_on)<= ? and i.project_id = ? and i.status_id in (1,2,4,7) group by author_id', due, project_id]
@@ -90,7 +89,7 @@ class ChartsController < ApplicationController
 
     
     ret_val = []
-
+    
     author_ids.each do |a|
       issues = Issue.find_by_sql ['select count(1) c ,DATE_FORMAT(i.created_on,"%Y-%m-%d") as create_date from issues i,users u where i.author_id = u.id and to_days(now())-to_days(i.created_on)<= ? and i.project_id = ? and i.status_id in (1,2,4,7) and author_id = ? group by create_date', due, project_id,a]
       data = Hash.new
@@ -98,21 +97,22 @@ class ChartsController < ApplicationController
         data.store(al,0)
       end
       issues.each do |i|
-        data[i.create_date] = i.c
+        data[i.create_date] =i.c
       end
+      
       tmp = []
-
-      data.each_key { |key|
-        tmp << data[key].to_i
-      }
+      data.sort.each do |d|
+        tmp << d[1].to_i
+      end
       ret_val << tmp
     end
-
+    
     
 
     x_labels_arr = []
     x_labels_arr << x_labels.reverse * '|'
 
+    
     [ret_val,legend,x_labels_arr]
   end
 
